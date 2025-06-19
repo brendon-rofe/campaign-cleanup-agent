e451202961727fb8ebaaa112ea09b9536f58e67f
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-06-19 10:38:18 +0200

    feat: Implement initial llm setup

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, world"}
+    response = llm.invoke("Sing a ballad of LangChain.")
+    return response
